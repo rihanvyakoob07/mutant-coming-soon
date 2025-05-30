b240adbc02e245d5474f995947cf9663bed6bf4b
rihanvyakoob07 <rihanvy32@gmail.com>
2025-05-30 06:44:34 +0530

    Update SocialLinks.tsx

--- src/components/landing/SocialLinks.tsx
+++ src/components/landing/SocialLinks.tsx
@@ -1,47 +1,6 @@
 
 
 
-
-
-
-
-// // SocialLinks.tsx
-// import React from 'react';
-// import { FaFacebookF, FaInstagram, FaLinkedinIn, FaYoutube } from 'react-icons/fa';
-
-// export const SocialLinks: React.FC = () => {
-//   const socialLinks = [
-//     { icon: <FaFacebookF className="text-white text-base lg:text-lg" />, href: '#', name: 'Facebook' },
-//     { icon: <FaInstagram className="text-white text-base lg:text-lg" />, href: '#', name: 'Instagram' },
-//     { icon: <FaLinkedinIn className="text-white text-base lg:text-lg" />, href: '#', name: 'LinkedIn' },
-//     { icon: <FaYoutube className="text-white text-base lg:text-lg" />, href: '#', name: 'YouTube' },
-//   ];
-
-//   return (
-//     <ul className="flex gap-4 justify-center">
-//       {socialLinks.map((link, index) => (
-//         <li key={index}>
-//           <a
-//             href={link.href}
-//             target="_blank"
-//             rel="noopener noreferrer"
-//             className="w-9 h-9 lg:w-10 lg:h-10 bg-[#4A4A4A] rounded-lg flex items-center justify-center hover:bg-[#5A5A5A] transition-colors duration-300 focus:outline-none"
-//             aria-label={`Visit our ${link.name} page`}
-//             title={link.name}
-//           >
-//             {link.icon}
-//           </a>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-
-
-
-
-
 /// SocialLinks.tsx
 import React from 'react';
 import { FaFacebookF, FaInstagram, FaLinkedinIn, FaYoutube } from 'react-icons/fa';
@@ -73,3 +32,10 @@ export const SocialLinks: React.FC = () => {
     </ul>
   );
 };
+
+
+
+
+
+
+
