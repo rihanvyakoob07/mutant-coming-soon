397d70a9bffa230b3f59b3c3ac08069e867bba54
rihanvyakoob07 <rihanvy32@gmail.com>
2025-06-02 01:54:14 +0530

    Update SocialLinks.tsx

--- src/components/landing/SocialLinks.tsx
+++ src/components/landing/SocialLinks.tsx
@@ -1,18 +1,36 @@
 
 
 
-/// SocialLinks.tsx
+
+
+
 
 /// SocialLinks.tsx
 import React from 'react';
-import { FaFacebookF, FaInstagram, FaLinkedinIn, FaYoutube } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaLinkedinIn, FaYoutube } from 'react-icons/fa';
 
 export const SocialLinks: React.FC = () => {
   const socialLinks = [
-    { icon: <FaFacebookF className="text-white text-lg sm:text-xl lg:text-2xl" />, href: '#', name: 'Facebook' },
-    { icon: <FaInstagram className="text-white text-lg sm:text-xl lg:text-2xl" />, href: '#', name: 'Instagram' },
-    { icon: <FaLinkedinIn className="text-white text-lg sm:text-xl lg:text-2xl" />, href: '#', name: 'LinkedIn' },
-    { icon: <FaYoutube className="text-white text-lg sm:text-xl lg:text-2xl" />, href: '#', name: 'YouTube' },
+    { 
+      icon: <FaFacebook className="text-white text-lg sm:text-xl lg:text-2xl" />, 
+      href: 'https://www.instagram.com/themutantschool/', 
+      name: 'Facebook' 
+    },
+    { 
+      icon: <FaInstagram className="text-white text-lg sm:text-xl lg:text-2xl" />, 
+      href: 'https://www.instagram.com/themutantschool/', 
+      name: 'Instagram' 
+    },
+    { 
+      icon: <FaLinkedinIn className="text-white text-lg sm:text-xl lg:text-2xl" />, 
+      href: 'https://www.linkedin.com/company/themutantschool/', 
+      name: 'LinkedIn' 
+    },
+    { 
+      icon: <FaYoutube className="text-white text-lg sm:text-xl lg:text-2xl" />, 
+      href: 'https://www.youtube.com/@TheMutantSchool', 
+      name: 'YouTube' 
+    },
   ];
 
   return (
@@ -22,7 +40,7 @@ export const SocialLinks: React.FC = () => {
           <a
             href={link.href}
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noopener noreferrer nofollow"
             className="w-10 h-10 sm:w-11 sm:h-11 lg:w-12 lg:h-12 bg-[#4A4A4A] rounded-lg flex items-center justify-center hover:bg-[#5A5A5A] transition-all duration-300 focus:outline-none active:scale-95 hover:scale-105"
             aria-label={`Visit our ${link.name} page`}
             title={link.name}
